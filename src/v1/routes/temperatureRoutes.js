ff43f662a0c5a9206a3a27d3d6cb0dcfaa95ca51
Lasith Hettiarachchi <lasith.hettiarachchi@h2o.ai>
2022-06-15 00:48:48 +0530

    Update workflow and adding lint

--- src/v1/routes/temperatureRoutes.js
+++ src/v1/routes/temperatureRoutes.js
@@ -1,29 +1,33 @@
-const express = require("express");
-const temperatureController = require("../../controllers/temperatureController");
-const router = express.Router();
+const express = require('express');
+const temperatureController = require('../../controller/temperatureController');
+const router = new express.Router();
 
 /**
  * @openapi
  * /api/v1/temperature:
  *   get:
- *       description: Get Temperatures
- *       responses:
- *       200:
- *           description: Success
+ *    description: Get Temperatures
+ *    responses:
+ *      200:
+ *        description: Success
  */
-router.get("/", temperatureController.getAllTemperatures);
+router.get('/', temperatureController.getAllTemperatures);
 
 /**
  * @openapi
- * /api/v1/temperature/{temperatureId}:
- *   get:
- *       description: Get Temperature
- *       parameters:
- *        - in: path
- *          name: temperatureId
- *          required: true
- *          type: integer
+ * /api/v1/temperature/{beerId}:
+ *  get:
+ *    description: Get Temperature
+ *    parameters:
+ *    - in: path
+ *    name: beerId
+ *    required: true
+ *    type: integer
+ *    responses:
+ *      200:
+ *        description: Success
+ *
  */
-router.get("/:temperatureId", temperatureController.getOneTemperature);
+router.get('/:beerId', temperatureController.getOneTemperature);
 
 module.exports = router;
--- src/v1/swagger.js
+++ src/v1/swagger.js
@@ -1,13 +1,13 @@
-const swaggerJSDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 // Basic Meta Information about our API
 const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: { title: "Beer Temperature API", version: "1.0.0" },
-    },
-    apis: ["./src/v1/routes/temperatureRoutes.js"],
+  definition: {
+    openapi: '3.0.0',
+    info: {title: 'Beer Temperature API', version: '1.0.0'},
+  },
+  apis: ['./src/v1/routes/temperatureRoutes.js'],
 };
 
 // Docs in JSON format
@@ -15,16 +15,16 @@ const swaggerSpec = swaggerJSDoc(options);
 
 // Function to setup our docs
 const swaggerDocs = (app, port) => {
-    // Route-Handler to visit our docs
-    app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-    // Make our docs in JSON format available
-    app.get("/api/v1/docs.json", (req, res) => {
-        res.setHeader("Content-Type", "application/json");
-        res.send(swaggerSpec);
-    });
-    console.log(
-        `Version 1 Docs are available on http://localhost:${port}/api/v1/docs`
-    );
+  // Route-Handler to visit our docs
+  app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  // Make our docs in JSON format available
+  app.get('/api/v1/docs.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
+  console.log(
+      `Version 1 Docs are available on http://localhost:${port}/api/v1/docs`,
+  );
 };
 
-module.exports = { swaggerDocs };
+module.exports = {swaggerDocs};
